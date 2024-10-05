0fe90182dd12c2e3f781054f2b48322025889311
Guh-paixao <gustavopaixao.dev@gmail.com>
2024-10-05 11:00:54 -0300

    dnv

--- Scenes/orc.gd
+++ Scenes/orc.gd
@@ -3,26 +3,21 @@ extends CharacterBody2D
 @export var health: int = 3
 @export var speed: float = 40.0
 
-
 @onready var player = get_node("/root/World/Player")
 @onready var anim = $AnimatedSprite2D
-
-func _ready() -> void:
-	play_walk()
 
 func _physics_process(_delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * speed
 	move_and_slide()
+	
+	if velocity != Vector2.ZERO and health >= 0 and anim.animation != "hurt":
+		anim.play("run_down")
 
 func take_damage():
 	health -= 1
 	anim.play("hurt")
 	
 	if health <= 0:
-		anim.play("dearth")
+		anim.play("death")
 		queue_free()
-
-func play_walk():
-	if velocity != Vector2.ZERO && health < 1:
-		anim.play("run_down")
--- bullet.gd
+++ bullet.gd
@@ -1,17 +1,18 @@
 extends Area2D
 
 var travelled_distance = 0
+@export var SPEED: float = 1000.0
+@export var RANGE: float = 1200.0
 
 func _physics_process(delta: float) -> void:
-	const SPEED = 1000
-	const RANGE = 1200
-	
 	var direction = Vector2.RIGHT.rotated(rotation)
 	position += direction * SPEED * delta
-
+	
 	travelled_distance += SPEED * delta
+	
 	if travelled_distance > RANGE:
 		queue_free()
+
 
 
 func _on_body_entered(body: Node2D) -> void:
